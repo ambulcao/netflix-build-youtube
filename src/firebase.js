c668a08b31b2a951d10c09c9e9024802564a1857
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-02-03 21:43:22 +0000

    Criando arquivos do projeto

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXDpuXoZAZgeTH-k5eubxSrJ20IfEL1-A",
+  authDomain: "netflix-clone-22e29.firebaseapp.com",
+  projectId: "netflix-clone-22e29",
+  storageBucket: "netflix-clone-22e29.appspot.com",
+  messagingSenderId: "767218640542",
+  appId: "1:767218640542:web:bbf3bfae9b48a921503d8f"
+};
