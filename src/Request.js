e06b5c86f333756d86b0905401d3a7dcd04adb47
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-02-04 18:48:37 +0000

    Adicionando Banner e API

--- src/Request.js
+++ src/Request.js
@@ -2,7 +2,7 @@
 
 const API_KEY = "49183a8f12913fd27e3b4358c53724cf";
 
-const request = {
+const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -11,4 +11,6 @@ const request = {
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, 
-}
+};
+
+export default requests;
