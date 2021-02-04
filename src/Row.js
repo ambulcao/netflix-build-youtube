1f7ba6c3ce2feecf0371bff690efdb301fc383d3
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-02-04 20:31:07 +0000

    Ajustando posters e atualização automatica do Banner

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import axios from "./axios";
 import "./Row.css";
-import axios from 'axios';
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
+
+    const base_url = "https://image.tmdb.org/t/p/original/";
 
     useEffect(() => {
         async function fetchData() {
@@ -11,14 +13,28 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             setMovies(request.data.results);
             return request;
         }
+
         fetchData();
-    }, [fetchUrl])
+    }, [fetchUrl]);
 
     return (
         <div className="row">
             <h2>{title}</h2>
+            <div className="row_posters">
+                {movies.map((movie) => (
+                    ((isLargeRow && movie.poster_path) || 
+                    (!isLargeRow && movie.backdrop_path)) && (
+                        <img 
+                            className={`row_poster ${isLargeRow && "row_posterLarge"}`} 
+                            key={movie.id} 
+                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                            alt={movie.name} 
+                        />
+                    ))
+                )}
+            </div>
         </div>
-    )
+    );
 }
-
+ 
 export default Row;
