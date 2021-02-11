b28fa1168d4b4f89d07754fbf895969bf98e8cee
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-02-11 21:09:46 +0000

    Configurando planos de assinatura

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import './PlansScreen.css';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 import db from '../firebase';
+import './PlansScreen.css';
+import { loadStripe } from "@stripe/stripe-js";
 
 function PlansScreen() {
 const [products, setProducts] = useState([]);
+const user = useSelector(selectUser);
 
 useEffect(() => {
     db.collection('products')
@@ -27,9 +31,43 @@ useEffect(() => {
 
     console.log(products);
 
+    const loadCheckout = async (priceId) => {
+        const docRef = await db
+            .collection('customers')
+            .doc(user.uid)
+            .collection("checkout_sessions")
+            .add({
+                price: priceId,
+                success_url: window.location.origin,
+                cancel_url: window.location.origin,
+            });
+        docRef.onSnapshot(async(snap) => {
+            const {error, sessionId} = snap.data();
+
+            if(error) {
+                alert(`An error occured: ${error.message}`);
+            }
+
+            if(sessionId) {
+                const stripe = await loadStripe("pk_test_51IJ30UFg5mMdWFjNVHKBwfnzol5f0fez6elN6l0oVn3PTSyGoj9nM5isEQwFooc3j0G1kYvo3pXqq0Pw0yjnnUXE00mcedT33I");
+                stripe.redirectToCheckout({ sessionId });
+            }
+        });
+    };
+
     return (
         <div className="plansScreen">
-
+            {Object.entries(products).map(([productId, productData]) => {
+               return(
+                   <div className="plansScreen_plans">
+                        <div className="plansScreen_info">
+                            <h5>{productData.name}</h5>
+                            <h6>{productData.description}</h6>
+                        </div>
+                        <button onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
+                   </div>
+               );
+            })}
         </div>
     )
 }
